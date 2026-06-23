f184484475e5e9bcb311ae9f260bd77429437403
Prisha Dureja <prisha.dureja@sportsfan360.com>
2026-06-23 16:42:41 +0530

    feat: select watchAlongRoomId in rooms API response

--- app/api/roar/rooms/route.ts
+++ app/api/roar/rooms/route.ts
@@ -69,7 +69,8 @@ export async function GET(req: NextRequest) {
         "fanCount",
         "scheduledStartTime",
         "score",
-        "scoreSubtitle"
+        "scoreSubtitle",
+        "watchAlongRoomId"
       )
       .get();
 
